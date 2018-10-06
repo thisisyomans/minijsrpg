3f9c63d6f8847bebdb3d560b05a4d80a5a545b93
thisisyomans <tanejamm@gmail.com>
2018-10-06 13:55:59 -0700

    need to add villages for visiting, movement not working for PlayerCharacter

--- src/game.js
+++ src/game.js
@@ -23,6 +23,9 @@ Game = {
 		Crafty.init(Game.width(), Game.height());
 		Crafty.background('rgb(249, 223, 125)');
 
+		//player character
+		Crafty.e('PlayerCharacter').at(5, 5);
+
 		//tree at every edge square
 		for (var x = 0; x < Game.map_grid.width; x++) {
 			for (var y = 0; y < Game.map_grid.height; y++) {
@@ -30,24 +33,10 @@ Game = {
 
 				if(at_edge){
 					//place tree at current tile
-					Crafty.e('2D, Canvas, Color')
-					.attr({
-						x: x * Game.map_grid.tile.width,
-						y: y * Game.map_grid.tile.height,
-						w: Game.map_grid.tile.width,
-						h: Game.map_grid.tile.height
-					})
-					.color('rgb(20, 125, 40)');
+					Crafty.e('Tree').at(x, y);
 				} else if (Math.random() < 0.06){
 					//place bush at current tile
-                                        Crafty.e('2D, Canvas, Color')
-                                        .attr({
-                                                x: x * Game.map_grid.tile.width,
-                                                y: y * Game.map_grid.tile.height,
-                                                w: Game.map_grid.tile.width,
-                                                h: Game.map_grid.tile.height
-                                        })
-                                        .color('rgb(20, 125, 40)');
+					Crafty.e('Bush').at(x, y);
 				}
 			}
 		}
